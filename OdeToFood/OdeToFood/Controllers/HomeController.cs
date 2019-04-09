c309fd4f1b4fe33957be65306d3d4bd88e61b7ec
Catrina, Ma <catrinamtr@gmail.com>
2019-04-09 12:09:40 -0400

    init solution

---
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            //ViewBag.Location = "Toronto, Canada";
+
+            var aboutModel = new AboutModel(); 
+            aboutModel.Name = "Catrina";
+            aboutModel.Location = "Toronto, Canada";
+
+            return View(aboutModel);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
