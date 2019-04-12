33a7e957cc355dac12422d63e98d46f0499a34aa
Catrina, Ma <catrinamtr@gmail.com>
2019-04-12 15:06:36 -0400

    small update

--- OdeToFood/OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/OdeToFood/Controllers/ReviewsController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OdeToFood.Models;
 
 namespace OdeToFood.Controllers
 {
     public class ReviewsController : Controller
     {
         // GET: Reviews
-        public ActionResult Index()  
+        public ActionResult Index()
         {
-            return View();
+            var model =
+                from r in _reviews
+                orderby r.Country
+                select r;
+
+
+            return View(model);
         }
 
         // GET: Reviews/Details/5
@@ -85,5 +92,34 @@ namespace OdeToFood.Controllers
                 return View();
             }
         }
+
+        static List<RestaurantReview> _reviews = new List<RestaurantReview>
+        {
+            new RestaurantReview()
+            {
+                Id = 1,
+                Name = "Cinnamon Club",
+                City = "London",
+                Country = "UK",
+                Rating = 10
+            },
+            new RestaurantReview()
+            {
+                Id = 2,
+                Name = "Marrakesh",
+                City = "D.C",
+                Country = "USA",
+                Rating = 10
+            },
+            new RestaurantReview()
+            {
+                Id = 3,
+                Name = "The House of Elliot",
+                City = "Ghent",
+                Country = "Belgium",
+                Rating = 10
+            }
+            
+        };
     }
 }
